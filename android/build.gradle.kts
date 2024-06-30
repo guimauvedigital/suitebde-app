b3f762e3512209405088ebf5f743295a79c106e5
NathanFallet <contact@nathanfallet.me>
2024-06-30 11:27:44 +0200

    ok why not

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -78,7 +78,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    implementation("io.coil-kt:coil-compose:2.4.0")
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
